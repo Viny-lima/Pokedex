2ce6aeddb9e5a5036de65fc40b648893d7fd2fcf
rackel-21 <rmor@discente.ifpe.edu.br>
2021-11-16 19:02:39 -0300

    New PokemonContext

---
+++ Pokédex/Context/PokemonContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pokedex.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokédex.Context
+{
+    public class PokemonContext:DbContext
+    {
+        public DbSet<Pokemon> Pokemons { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=LojaDB;Trusted_Connection=true;");
+        }
+    }
+}
