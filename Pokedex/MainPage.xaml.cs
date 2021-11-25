f7c33870b0bff7de0cfeba858cdb638bcc046de4
Viny-lima <viniciuslima.vf4@hotmail.com>
2021-11-25 00:41:27 -0300

    Review Models xaml

--- Pokedex/MainPage.xaml.cs
+++ Pokedex/MainPage.xaml.cs
@@ -1,30 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+﻿using Pokedex.Model;
+using Pokedex.Model.PokeApi;
+using Pokedex.ViewModels;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// O modelo de item de Página em Branco está documentado em https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x416
 
 namespace Pokedex
 {
-    /// <summary>
-    /// Uma página vazia que pode ser usada isoladamente ou navegada dentro de um Quadro.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
+        public PokemonViewModelList PokeList = new PokemonViewModelList(0, 10);
+        public PokemonViewModel PokeView = new PokemonViewModel();
+
+
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Next_Pokemon_Click(object sender, RoutedEventArgs e)
+        {
+            PokeList.NextPageListPokemons();
+        }
+
+        private void Previus_Pokemon_Click(object sender, RoutedEventArgs e)
+        {
+            PokeList.PreviosPageListPokemons();
+        }
+
+        private void ListView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            var pokemonSelected = e.ClickedItem as PokemonAddress;
+
+            PokeView.Pokemon = ApiRequest.Get<Pokemon>(pokemonSelected.Url);
         }
     }
 }
