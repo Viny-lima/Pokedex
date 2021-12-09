fbc014ce4ee79fa5f75ad8e8f2d4c53dd9d231a0
ricardotenorio <ricardo.heber@gmail.com>
2021-12-08 21:08:40 -0300

    criada a interface IPokemonService

---
+++ Pokedex.Model/Service/IPokemonService.cs
@@ -0,0 +1,19 @@
+﻿using Pokedex.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokedex.Model.Service
+{
+    public interface IPokemonService<T> where T : class, IEntity
+    {
+        Task AddPokemon(T pokemon);
+        Task UpdatePokemon(T pokemon);
+        Task DeletePokemon(T pokemon);
+        Task<IList<T>> FindPokemons();
+        Task<IList<T>> FindPokemonsByType(string type);
+        Task<T> FindPokemonByName(string name);
+        Task<T> FindPokemonById(int id);
+    }
+}
