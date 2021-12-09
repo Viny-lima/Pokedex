12be8a92be35c04261385f12b717c8bec7f04eff
ricardotenorio <ricardo.heber@gmail.com>
2021-12-09 00:19:22 -0300

    implementação parcial da classe PokemonService

---
+++ Pokedex.Model/Service/PokemonService.cs
@@ -0,0 +1,61 @@
+﻿using Pokedex.Model.DAO;
+using Pokedex.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokedex.Model.Service
+{
+    class PokemonService : IPokemonService<PokemonDB>
+    {
+        private IDAO<PokemonDB> _pokemonDAO;
+
+        public PokemonService()
+        {
+            _pokemonDAO = new PokemonDAO();
+        }
+
+        public async Task AddPokemon(PokemonDB pokemon)
+        {
+            await _pokemonDAO.Add(pokemon);
+        }
+       
+        public async Task UpdatePokemon(PokemonDB pokemon)
+        {
+            await _pokemonDAO.Update(pokemon);
+        }
+
+        public async Task DeletePokemon(PokemonDB pokemon)
+        {
+            await _pokemonDAO.Delete(pokemon);
+        }
+
+        public async Task<PokemonDB> FindPokemonById(int id)
+        {
+            var pokemonFound = await ((PokemonDAO)_pokemonDAO).FindById(id);
+
+            if (pokemonFound == null)
+            {
+                
+            }
+
+            return pokemonFound;
+        }
+
+        public async Task<PokemonDB> FindPokemonByName(string name)
+        {
+            return await ((PokemonDAO)_pokemonDAO).FindByName(name);
+        }
+
+        public Task<IList<PokemonDB>> FindPokemons()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<PokemonDB>> FindPokemonsByType(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Pokemon.Migrations.Startup/Program.cs
+++ Pokemon.Migrations.Startup/Program.cs
@@ -10,11 +10,8 @@ namespace Pokedex.Migrations.Startup
     {
         static void Main(string[] args)
         {
-            var pokemonDAO = new PokemonDAO();
 
-            var p = pokemonDAO.FindById(1).Result;
-
-            Console.WriteLine(p.Types[0].Type.Name);
+            MostrarPokemons();
 
         }
 
@@ -22,7 +19,7 @@ namespace Pokedex.Migrations.Startup
         {          
             var pokemonDAO = new PokemonDAO();
 
-            for (int i = 1; i < 10; i++)
+            for (int i = 100; i < 111; i++)
             {
                 var p = new PokemonDB(ApiRequest.GetPokemon(i));
                 pokemonDAO.Add(p);
@@ -41,6 +38,27 @@ namespace Pokedex.Migrations.Startup
                     Console.Write(type.Type.Name + " / ");
                 }
                 Console.WriteLine("");
+            }
+        }
+        
+        private static void MostrarPokemons()
+        {
+            var db = new PokemonDAO();
+
+            foreach (var pokemon in db.FindAll().Result)
+            {
+                Console.WriteLine("---------------------------");
+                Console.WriteLine(pokemon.Id);
+                Console.WriteLine(pokemon.Name);
+                Console.WriteLine(pokemon.Height);
+                Console.WriteLine(pokemon.Weight);
+                Console.WriteLine(pokemon.Hp);
+                Console.WriteLine(pokemon.Attack);
+                Console.WriteLine(pokemon.SpecialAttack);
+                Console.WriteLine(pokemon.Defense);
+                Console.WriteLine(pokemon.SpecialDefense);
+                Console.WriteLine(pokemon.Speed);
+                Console.WriteLine("---------------------------");
             }
         }
 
