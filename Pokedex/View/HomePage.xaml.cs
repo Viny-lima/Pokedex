5dd68ee6edffc295a7f54a01bace3653ac7cc18a
Vinicius lima <viniciuslima.vf4@hotmail.com>
2022-01-09 17:33:24 -0300

    Popup na Home Page

--- Pokedex/View/HomePage.xaml.cs
+++ Pokedex/View/HomePage.xaml.cs
@@ -1,15 +1,16 @@
 ﻿using Pokedex.Model.DAO;
 using Pokedex.Model.Entities;
 using Pokedex.Model.Enums;
+using Pokedex.Model.Exceptions;
 using Pokedex.Model.Service;
 using Pokedex.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
-using System.Text.RegularExpressions;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
 
 namespace Pokedex.View
 {
@@ -19,6 +20,8 @@ namespace Pokedex.View
         private PokemonService _service = new PokemonService();
         private PokemonDB _pokemon;
         private List<String> _listaNamesPokemons;
+        private SolidColorBrush fundoEscuro = new SolidColorBrush(Windows.UI.Color.FromArgb(178, 0, 0, 0));
+        private SolidColorBrush fundoClaro = new SolidColorBrush(Windows.UI.Color.FromArgb(120, 0, 0, 0));
         
         public HomePage()
         {
@@ -71,20 +74,27 @@ namespace Pokedex.View
 
             }
             catch (ArgumentException)
+            {                
+                ERROR.Text = "ERROR: invalid search";
+                OpenPopup();
+            }                        
+            catch (AggregateException aggregateException)
             {
-                ERROR.Visibility = Visibility.Visible;
-                ERROR.Text = "ERROR: This query doesn't exist";
-            }
-            
-            catch (NullReferenceException)
-            {
-                ERROR.Visibility = Visibility.Visible;
-                ERROR.Text = "ERROR: Null Reference Exception";
-            }
-            catch (Exception)
-            {
-                ERROR.Visibility = Visibility.Visible;
-                ERROR.Text = "ERROR: Pokemon not Found";
+                foreach (var e in aggregateException.InnerExceptions)
+                {
+                    // Handle the custom exception.
+                    if (e is PokemonNotFoundException)
+                    {                        
+                        ERROR.Text = "ERROR: Pokemon not Found";
+                    }
+                    // Rethrow any other exception.
+                    else
+                    {
+                        ERROR.Text = "ERROR: aggregate exception";
+                    }
+                }
+
+                OpenPopup();
             }
             finally
             {
@@ -93,7 +103,21 @@ namespace Pokedex.View
                     RootFrame.Navigate(typeof(PokemonPage), _pokemon.Id);
                 }
             }
-        }  
+        }
+
+        private void ButtonClosePopup_Click(object sender, RoutedEventArgs e)
+        {
+            ShadowPage.Fill = fundoClaro;
+            PopupError.IsOpen = !PopupError.IsOpen;
+        }
+
+        private void OpenPopup()
+        {
+            ShadowPage.Fill = fundoEscuro;
+            PopupError.IsOpen = !PopupError.IsOpen;
+        }
+
+
     }
 
 }
